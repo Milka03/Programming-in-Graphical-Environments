b6b858968f5b9e9e80e19bca4266840891459be2
unknown <emiliamwroblewska@gmail.com>
2020-06-25 10:52:29 +0200

    Added via upload

---
+++ WPF/Wpf_Lab2_Home/Wpf_Lab2_Home/MainPage1.xaml.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using Wpf_Lab2.Pages;
+//using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Wpf_Lab2_Home
+{
+    /// <summary>
+    /// Interaction logic for MainPage1.xaml
+    /// </summary>
+    public partial class MainPage1 : Page
+    {
+        MainWindow main;
+        GlobalData globalData;
+        List<ImagePage> imagePages;
+        List<DirectoryPage> directoryPages;
+        List<PasswordPage> passwordPages;
+        //public string MyImageSource { get; set; }
+
+        public TreeViewItem LastTitem { get; set; }
+        public bool isInEditMode { get; set; }
+        public MainPage1()
+        {
+            InitializeComponent();
+            main = (MainWindow)App.Current.MainWindow;
+            globalData = new GlobalData();
+            imagePages = new List<ImagePage>();
+            directoryPages = new List<DirectoryPage>();
+            passwordPages = new List<PasswordPage>();
+
+            this.DataContext = main;
+            treevMenu.Items.Clear();
+            isInEditMode = false;
+        }
+        
+        
+        private void Trv_SelectedItemChanged(object sender, MouseButtonEventArgs e)
+        {
+            TreeViewItem it = (TreeViewItem)(sender);
+            string s = it.Header.ToString();
+            globalData.Current = (myData)it.Tag;
+            it.Focus();
+            LastTitem = it;
+
+            if (it.Tag is myImage)
+            {
+                (it.Tag as myImage).myPAGE.ImageGrid.Visibility = Visibility.Visible;
+                RightPanel.NavigationService.Navigate((it.Tag as myImage).myPAGE);
+            }
+            if (it.Tag is myDirectory)
+            {
+                (it.Tag as myDirectory).myPAGE.DirTextBlock.Text = s + "(" + it.Items.Count + ")";
+                RightPanel.NavigationService.Navigate((it.Tag as myDirectory).myPAGE);
+
+            }
+            if (it.Tag is myPassword)
+            {
+                RightPanel.NavigationService.Navigate((it.Tag as myPassword).myPAGE);
+            }
+
+        }
+       
+       
+        private void MenuLogout_Click(object sender, RoutedEventArgs e)
+        {
+            //main.LockPage
+            directoryPages.Clear();
+            passwordPages.Clear();
+            imagePages.Clear();
+            main.Framestart.NavigationService.Navigate(new StartPage());
+        }
+
+
+
+        //--------------------- Adding TreeViewItems ------------------------------
+        private void NewDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            myDirectory dir = new myDirectory();
+            TreeViewItem item = new TreeViewItem();
+            item.Header = dir.Name;
+            
+            item.FontWeight = FontWeights.Bold;
+            item.ContextMenu = this.Resources["DirectoryMenu"] as ContextMenu;
+
+            MenuItem mi = new MenuItem();
+            mi.Header = dir.Name;
+            mi.IsEnabled = false;
+            item.ContextMenu.Items[0] = mi;
+            TreeViewItem selected = (TreeViewItem)treevMenu.SelectedItem;
+            if (selected != null) selected.Items.Add(item);
+            else treevMenu.Items.Add(item);
+
+            DirectoryPage pd = new DirectoryPage(dir.Counter);
+            dir.myPAGE = pd;
+            globalData.Current = dir;
+            pd.DataContext = globalData;
+            directoryPages.Add(pd);
+            item.Tag = dir;
+
+            LastTitem = item;
+        }
+
+        private void NewPasswords_Click(object sender, RoutedEventArgs e)
+        {
+            myPassword pas = new myPassword();
+            TreeViewItem item = new TreeViewItem();
+            item.Header = pas.Name;
+            item.Tag = pas;
+            item.FontWeight = FontWeights.Normal;
+            item.FontStyle = FontStyles.Italic;
+
+            item.ContextMenu = this.Resources["ImagePasswordMenu"] as ContextMenu;
+            MenuItem mi = new MenuItem();
+            mi.Header = pas.Name;
+            mi.IsEnabled = false;
+            item.ContextMenu.Items[0] = mi;
+
+            TreeViewItem selected = (TreeViewItem)treevMenu.SelectedItem;
+            if (selected != null)
+            {
+                string s = selected.Header.ToString();
+                if (selected.Tag is myDirectory) selected.Items.Add(item);
+            }
+            else treevMenu.Items.Add(item);
+
+            PasswordPage pp = new PasswordPage(pas.Counter);
+            pas.myPAGE = pp;
+            globalData.Current = pas;
+            pp.DataContext = globalData;
+            passwordPages.Add(pp);
+            //RightPanel.NavigationService.Navigate(p2);
+            LastTitem = item;
+        }
+
+        private void NewImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select image";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" + "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                myImage img = new myImage();
+                TreeViewItem item = new TreeViewItem();
+                item.Header = img.Name;
+                item.Tag = img;
+                item.FontWeight = FontWeights.Normal;
+                item.FontStyle = FontStyles.Italic;
+                
+                item.ContextMenu = this.Resources["ImagePasswordMenu"] as ContextMenu;
+                MenuItem mi = new MenuItem();
+                mi.Header = img.Name;
+                mi.IsEnabled = false;
+                item.ContextMenu.Items[0] = mi;
+
+                TreeViewItem selected = (TreeViewItem)treevMenu.SelectedItem;
+                if (selected != null)
+                {
+                    string s = selected.Header.ToString();
+                    if (selected.Tag is myDirectory) selected.Items.Add(item);
+                }
+                else treevMenu.Items.Add(item);
+
+                ImagePage p = new ImagePage(img.Counter);
+                p.myImage.Source = new BitmapImage(new Uri(op.FileName));
+                p.ImageGrid.Visibility = Visibility.Collapsed;
+                img.myPAGE = p;
+                globalData.Current = img;
+                p.DataContext = globalData;
+                imagePages.Add(p);
+                //System.Windows.MessageBox.Show("Added to list " + imagePages.Count );
+                LastTitem = item;
+            }
+        }
+   
+
+        //--------------------- Rename TreeViewItem --------------------- not finished
+        //TextBox tempTextBox;
+        //TreeViewItem titem;
+        private void RenameDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem titem = (TreeViewItem)treevMenu.SelectedItem;
+            if (titem != null)
+            {
+                isInEditMode = true;
+                // the current node has focus
+                titem.Focus();
+                // no longer handle the operating system
+                e.Handled = true;
+            }            
+        }
+       
+        private void renametextbox_LostFous(object sender, RoutedEventArgs e)
+        {
+            //tempTextBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void ConfirmChangesByEnter(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        //---------------------- Delete TreeViewItem --------------------------
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)treevMenu.SelectedItem;
+            if (item != null)
+            {
+                TreeViewItem parent = GetParentObject(item);
+                if (parent == null) treevMenu.Items.Remove(item);
+                else parent.Items.Remove(item);
+            }
+        }
+
+       
+        public TreeViewItem GetParentObject(TreeViewItem obj)
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+            while (parent != null)
+            {
+                if (parent is TreeViewItem)
+                {
+                    return (TreeViewItem)parent;
+                }
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return null;
+        }
+
+        private void TreeView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right && !(e.OriginalSource is TextBlock))
+            {
+                TreeViewItem item = (TreeViewItem)treevMenu.SelectedItem;
+                if (item != null)
+                {
+                    treevMenu.Focus();
+                    item.IsSelected = false;
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ WPF/Wpf_Lab2_Home/Wpf_Lab2_Home/PasswordPage.xaml.cs
@@ -0,0 +1,302 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace Wpf_Lab2_Home
+{
+    /// <summary>
+    /// Interaction logic for PasswordPage.xaml
+    /// </summary>
+    public partial class PasswordPage : Page
+    {
+        public List<PasswordItem> xp { get; set; }
+
+        public PasswordItem LastItem;
+        public CollectionView collectionView;
+        public PropertyGroupDescription groupDescription;
+        public int ID { get; set; }
+        public PasswordPage(int name)
+        {
+            InitializeComponent();
+            ID = name;
+            xp = new List<PasswordItem>();
+            PasswordList.ItemsSource = xp.ToArray();
+
+            collectionView = (CollectionView)CollectionViewSource.GetDefaultView(PasswordList.ItemsSource);
+            groupDescription = new PropertyGroupDescription("FirstLetter");
+            collectionView.GroupDescriptions.Add(groupDescription);
+            //collectionView.Filter += UserFilter;
+            PasswordList.Items.SortDescriptions.Add(new SortDescription("NameBox", ListSortDirection.Ascending));
+        }
+
+        private void AddPasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordItem viewItem = new PasswordItem(System.DateTime.Now);
+            viewItem.NameBox = "Account Name";
+            //viewItem.Height = 80;
+            //viewItem.Content = viewItem.NameBox;
+
+            PasswordList.SelectedItem = viewItem;
+            xp.Add(viewItem);
+            PasswordList.ItemsSource = xp.ToArray();
+            PasswordList.Items.Refresh();
+            //((this.DataContext as GlobalData).Current as myPassword).listOfPasswords.Add(viewItem);
+            (this.DataContext as GlobalData).IsVisibleForm = Visibility.Visible;
+            (this.DataContext as GlobalData).IsVisibleViewer = Visibility.Hidden;
+            this.FormEditor.Visibility = Visibility.Visible;
+            this.FormViewer.Visibility = Visibility.Hidden;
+            (this.DataContext as GlobalData).NotifyPropertyChanged("isVisibleForm");
+
+            if (SelectbtnImage != null) SelectbtnImage.Source = null;
+            this.AddButton.IsEnabled = false;
+            this.ListSearchBox.IsEnabled = false;
+            LastItem = viewItem;
+            PasswordList.Focusable = false;
+            PasswordList.Items.SortDescriptions.Add(new SortDescription("NameBox", ListSortDirection.Ascending));
+            collectionView = (CollectionView)CollectionViewSource.GetDefaultView(PasswordList.ItemsSource);
+            //groupDescription = new PropertyGroupDescription("NameBox");
+            collectionView.GroupDescriptions.Add(groupDescription);
+        }
+
+        private void SelectedItemChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PasswordItem pass = PasswordList.SelectedItem as PasswordItem;
+            if (pass == null) pass = LastItem;
+            //PasswordList.Items.Add(pass);
+
+            if (pass != null)
+            {
+                LastItem = pass;
+                this.FormViewer.Visibility = Visibility.Visible;
+                this.FormEditor.Visibility = Visibility.Hidden;
+                (this.DataContext as GlobalData).NotifyPropertyChanged("Current");
+            }
+        }
+
+        private void MouseOnFormHandler(object sender, MouseButtonEventArgs e)
+        {
+            if (PasswordList.SelectedItem != null) PasswordList.Focus();
+        }
+
+        //----------------------- Search Box and searching ---------------------------
+        private void SearchBoxClick(object sender, RoutedEventArgs e)
+        {
+            TextBox b = (TextBox)sender;
+            b.Text = "";
+            b.Foreground = Brushes.Black;
+        }
+
+        private void SearchBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox b = (TextBox)sender;
+            b.Text = "Search...";
+            b.Foreground = Brushes.DarkGray;
+            if (PasswordList != null) PasswordList.Focus();
+        }
+
+        private void SearchTextChaged(object sender, TextChangedEventArgs e)
+        {
+            TextBox box = (TextBox)sender;
+            if(this.FormViewer != null)
+                this.FormViewer.Visibility = Visibility.Hidden;
+            if(PasswordList != null)
+                CollectionViewSource.GetDefaultView(PasswordList.ItemsSource).Refresh();
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(this.ListSearchBox.Text) || this.ListSearchBox.Text == "Search...")
+                return true;
+            if (this.FormEditor.Visibility == Visibility.Visible) return true;
+            else
+            {
+                bool result = ((item as PasswordItem).NameBox.StartsWith(ListSearchBox.Text, StringComparison.OrdinalIgnoreCase));
+                if (result == false) this.FormViewer.Visibility = Visibility.Hidden;
+                return result;
+            }
+               
+        }
+
+        //----------------------- BUTTON CLICKS ----------------------
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordItem pass = PasswordList.SelectedItem as PasswordItem;
+            if (pass == null) pass = LastItem;
+
+            if (pass != null)
+            {
+                //pass.UpdateValues(box.Name, box.Text);
+                pass.NameBox = NameBox.Text;
+                pass.Email = EmailBox.Text;
+                pass.Login = LoginBox.Text;
+                pass.Password = PassBox.Text;
+                pass.Website = WebsiteBox.Text;
+                pass.Notes = NotesBox.Text;
+                pass.EditDate = DateTime.Now;
+                pass.FirstLetter = NameBox.Text[0].ToString();
+                pass.Icon = (BitmapImage)SelectbtnImage.Source;
+                PasswordList.Items.Refresh();
+
+                (this.DataContext as GlobalData).IsVisibleViewer = Visibility.Visible;
+                (this.DataContext as GlobalData).IsVisibleForm = Visibility.Hidden;
+                (this.DataContext as GlobalData).NotifyPropertyChanged("Current");
+
+                this.FormEditor.Visibility = Visibility.Hidden;
+                this.FormViewer.Visibility = Visibility.Visible;
+                this.AddButton.IsEnabled = true;
+                this.ListSearchBox.IsEnabled = true;
+                LastItem = pass;
+                PasswordList.Focusable = true;
+                PasswordList.Items.SortDescriptions.Add(new SortDescription("NameBox", ListSortDirection.Ascending));
+                collectionView = (CollectionView)CollectionViewSource.GetDefaultView(PasswordList.ItemsSource);
+                collectionView.Filter += UserFilter;
+            }
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            (this.DataContext as GlobalData).IsVisibleViewer = Visibility.Visible;
+            (this.DataContext as GlobalData).IsVisibleForm = Visibility.Hidden;
+            (this.DataContext as GlobalData).NotifyPropertyChanged();
+            this.FormEditor.Visibility = Visibility.Hidden;
+            this.FormViewer.Visibility = Visibility.Visible;
+            PasswordList.SelectedItem = LastItem;
+            this.AddButton.IsEnabled = true;
+            this.ListSearchBox.IsEnabled = true;
+            PasswordList.Focusable = true;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            (this.DataContext as GlobalData).IsVisibleViewer = Visibility.Hidden;
+            (this.DataContext as GlobalData).IsVisibleForm = Visibility.Visible;
+            (this.DataContext as GlobalData).NotifyPropertyChanged();
+
+            this.FormEditor.Visibility = Visibility.Visible;
+            this.FormViewer.Visibility = Visibility.Hidden;
+            PasswordItem pas = (PasswordItem)PasswordList.SelectedItem;
+            if (pas == null) pas = LastItem;
+            if (pas.Icon != null) SelectbtnImage.Source = pas.Icon;
+            if (pas.Icon == null) SelectbtnImage.Source = null;
+            this.AddButton.IsEnabled = false;
+            this.ListSearchBox.IsEnabled = false;
+            PasswordList.Focusable = false;
+        }
+
+        private void DeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            PasswordItem pas = PasswordList.SelectedItem as PasswordItem;
+            if (pas == null) pas = LastItem;
+            ((this.DataContext as GlobalData).Current as myPassword).listOfPasswords.Remove(pas);
+            (this.DataContext as GlobalData).NotifyPropertyChanged("Current");
+
+            this.xp.Remove(pas);
+            PasswordList.ItemsSource = xp.ToArray();
+            PasswordList.Items.Refresh();
+            this.FormEditor.Visibility = Visibility.Hidden;
+            this.FormViewer.Visibility = Visibility.Hidden;
+            this.AddButton.IsEnabled = true;
+            this.ListSearchBox.IsEnabled = true;
+            PasswordList.Focusable = true;
+
+            PasswordList.Items.SortDescriptions.Add(new SortDescription("NameBox", ListSortDirection.Ascending));
+            collectionView = (CollectionView)CollectionViewSource.GetDefaultView(PasswordList.ItemsSource);
+            //groupDescription = new PropertyGroupDescription("NameBox");
+            collectionView.GroupDescriptions.Add(groupDescription);
+            collectionView.Filter += UserFilter;
+        }
+
+        //---------------------------------------------------------------------------------
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select image";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                SelectbtnImage.Source = new BitmapImage(new Uri(op.FileName));
+                //PasswordItem pass = PasswordList.SelectedItem as PasswordItem; ->Previoulsy for lab part
+                //if (pass == null) PasswordList.SelectedItem = pass;
+                //pass.Icon = new BitmapImage(new Uri(op.FileName));
+
+                (this.DataContext as GlobalData).NotifyPropertyChanged("Current");
+            }
+        }
+
+        private void FormTextChangedHandler(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        //--------------------- Viewer functionalities -------------------
+
+        private void CopyToClipboard(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            PasswordItem pas = PasswordList.SelectedItem as PasswordItem;
+            if (pas == null) pas = LastItem;
+            string tmp = "";
+            if (b.Name == "Copybtn1") tmp = pas.Email;
+            if (b.Name == "Copybtn2") tmp = pas.Login;
+            if (b.Name == "Copybtn3") tmp = pas.Password;
+            Clipboard.SetText(tmp);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            string s = LastItem.Website;
+            try
+            {
+                System.Diagnostics.Process.Start(s);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            e.Handled = true;
+        }
+
+        private void EmailHyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Hyperlink hyperlink = sender as Hyperlink;
+            if (hyperlink == null) return;
+            if (Regex.IsMatch(hyperlink.NavigateUri.ToString(), @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+            {
+                string address = string.Concat("mailto:", hyperlink.NavigateUri.ToString());
+                try { System.Diagnostics.Process.Start(address); }
+                catch { MessageBox.Show("That e-mail address is invalid.", "E-mail error"); }
+            }
+
+            //try
+            //{
+            //    if (Regex.IsMatch(hyperlink.NavigateUri.ToString(), @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")) {}
+            //    else { MessageBox.Show("Invalid e-mail format"); }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+        }
+
+    }
+}
